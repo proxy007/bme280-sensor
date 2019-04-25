a628f2dd4bd354b80f9262c9f7e71f027ff1628d
proxy007 <warmaster007@gmail.com>
2019-04-25 14:22:55 +0200

    Configuration
    - Configured normal mode to 1000ms
    - Deactivated Pressure measurement
    - Changed Oversampling to 8x (temp & hum)

--- BME280.js
+++ BME280.js
@@ -47,6 +47,7 @@ class BME280 {
 
     this.REGISTER_CONTROL_HUM   = 0xF2;
     this.REGISTER_CONTROL       = 0xF4;
+    this.REGISTER_CONFIG        = 0xF5;
     this.REGISTER_PRESSURE_DATA = 0xF7;
     this.REGISTER_TEMP_DATA     = 0xFA;
     this.REGISTER_HUMIDITY_DATA = 0xFD;
@@ -77,17 +78,25 @@ class BME280 {
                 return reject(err);
               }
 
-              // Humidity 16x oversampling
+              // Config Normal mode to 1000ms
               //
-              this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL_HUM, 0b00000101, (err) => {
+              this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONFIG, 0b01010010, (err) => {
                 if(err) {
                   return reject(err);
                 }
-
-                // Temperture/pressure 16x oversampling, normal mode
+              
+                // Humidity 8x oversampling
                 //
-                this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL, 0b10110111, (err) => {
-                  return err ? reject(err) : resolve(chipId);
+                this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL_HUM, 0b00000100, (err) => {
+                  if(err) {
+                    return reject(err);
+                  }
+
+                  // Pressure OFF, Temperture 8x oversampling, normal mode
+                  //
+                  this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL, 0b10000011, (err) => {
+                    return err ? reject(err) : resolve(chipId);
+                  });
                 });
               });
             });
